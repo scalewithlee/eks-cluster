f9d5170b02d1e52e205391a7a7603a6b1b607cd3
lee <leetgaines@gmail.com>
2025-05-09 06:45:33 -0400

    add missing variables

--- infrastructure/terraform/modules/eks/variables.tf
+++ infrastructure/terraform/modules/eks/variables.tf
@@ -1,5 +1,54 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
+variable "cluster_version" {
+  description = "Kubernetes version"
+  type        = string
+  default     = "1.32"
+}
+
+variable "environment" {
+  description = "Environment to deploy to"
+  type        = string
+  default     = "dev"
+}
+
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC where the EKS cluster will be deployed"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "IDs of the subnets within the VPC"
+  type        = list(string)
+}
+
+variable "ami_type" {
+  description = "The type of AMI the EKS nodes will use"
+  type        = string
+  default     = "AL2023_x86_64_STANDARD"
+}
+
 variable "node_security_group_id" {
   description = "ID of the security group for the EKS nodes"
+  type        = string
+}
+
+variable "cluster_security_group_id" {
+  description = "ID of the security group for the EKS cluster"
   type        = string
 }
 
@@ -13,6 +62,11 @@ variable "cluster_endpoint_private_access" {
   description = "Whether or not to enable private API endpoints for the cluster"
   type        = bool
   default     = false
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs for the EKS cluster"
+  type        = list(string)
 }
 
 variable "node_group_min_size" {
