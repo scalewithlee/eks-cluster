7aa73ea7dc6a2469d852c0d7cc80d92f6166dc9a
lee <leetgaines@gmail.com>
2025-05-10 19:15:11 -0400

    add dev backend (generated from bootstrap)

--- infrastructure/terraform/environments/dev/backend.tf
+++ infrastructure/terraform/environments/dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "tf-state-bucket-1746822481"
-    key            = "dev/terraform.tfstate"
-    region         = "us-west-2"
-    encrypt        = true
-    use_lockfile   = true  # Use S3 native state locking
+    bucket       = "tf-state-bucket-1746822481"
+    key          = "dev/terraform.tfstate"
+    region       = "us-west-2"
+    encrypt      = true
+    use_lockfile = true # Use S3 native state locking
   }
 }
